c1603ffa87ef7799438eb20e72b8866e8e92768d
Axel Van Gestel <r0784084@student.thomasmore.be>
2022-11-21 12:51:54 +0100

    Made drive.py not record by default; Added some opencv test code

--- drive.py
+++ drive.py
@@ -1,45 +1,51 @@
-import base64  # for lossless encoding transfer
+import argparse  # parsing command line arguments
+import shutil  # high level file operations
+import base64  # decoding camera images
 import os  # write + read files
 import random
 from datetime import datetime  # to set frame timestamp
 from io import BytesIO  # manipulate string and byte data in memory
 
 import cv2
-import eventlet.wsgi
-import numpy as np
-import socketio  # server
-from PIL import Image
+import eventlet.wsgi  # web server gateway interface
+import numpy as np  # matrix math
+import socketio  # real-time server
+from PIL import Image  # image manipulation
 from flask import Flask  # framework for web devices
-from keras.models import load_model
 
+# from keras.models import load_model  # load our saved model
+
+# Set resulting image width & height
 height = 320
 width = 160
+
+# set min/max speed for our autonomous car
+max_speed = 30
+min_speed = 10
+
+# and a speed limit
+speed_limit = max_speed
 
 
 def resize(image):
     return cv2.resize(image, (width, height), cv2.INTER_AREA)
 
 
-# server init
+# initialize our server
 sio = socketio.Server(always_connect=True)
 # flask web app
 application = Flask(__name__)
 
-# init empty model and image array
+# init our model and image array as empty
 net = None
 image_array_before = None
-
-# Speed limits
-max_speed = 30
-min_speed = 10
-
-speed_limit = max_speed
 
 
 # Server event handler
 @sio.on('telemetry')
 def telemetry(sid, data):
     if data:
+        # Read data Unity sends us
         steering_angle = float(data["steering_angle"].replace(",", "."))
         throttle = float(data["throttle"].replace(",", "."))
         speed = float(data["speed"].replace(",", "."))
@@ -49,6 +55,12 @@ def telemetry(sid, data):
         timestamp = datetime.utcnow().strftime('%Y_%m_%d_%H_%M_%S_%f')[:-3]
         image_filename = os.path.join(r'./path', timestamp)
         image.save('{}.jpg'.format(image_filename))
+
+        # save frame
+        if args.image_folder != '':
+            timestamp = datetime.utcnow().strftime('%Y_%m_%d_%H_%M_%S_%f')[:-3]
+            image_filename = os.path.join(args.image_folder, timestamp)
+            image.save('{}.jpg'.format(image_filename))
 
         try:
             image = np.asarray(image)
@@ -97,7 +109,38 @@ def send_control(steering_angle, throttle):
 
 
 if __name__ == "__main__":
-    # net = load_model('path')
+    parser = argparse.ArgumentParser(description='Remote Driving')
+    parser.add_argument(
+        'model',
+        type=str,
+        nargs='?',
+        default='model.h5',
+        help='Path to model h5 file. Model should be on the same path.'
+    )
+    parser.add_argument(
+        'image_folder',
+        type=str,
+        nargs='?',
+        default='',
+        help='Path to image folder. This is where the images from the run will be saved.'
+    )
+    args = parser.parse_args()
+
+    # load model
+    # model = load_model(args.model)
+
+    if args.image_folder != '':
+        print("Creating image folder at {}".format(args.image_folder))
+        if not os.path.exists(args.image_folder):
+            os.makedirs(args.image_folder)
+        else:
+            shutil.rmtree(args.image_folder)  # Could be dangerous
+            os.makedirs(args.image_folder)
+        print("RECORDING THIS RUN ...")
+    else:
+        print("NOT RECORDING THIS RUN ...")
+
+    # wrap Flask application with engineio's middleware
     application = socketio.Middleware(sio, application)
-    # deploy
+    # deploy as an eventlet WSGI server
     eventlet.wsgi.server(eventlet.listen(('localhost', 4567)), application)
--- opencv.py
+++ opencv.py
@@ -1,38 +1,69 @@
 import cv2
-cv2.__version__
 import numpy as np
 import matplotlib.pyplot as plt
 
-img_orig = cv2.imread('resources/dog.jpg')
-img_rgb = cv2.cvtColor(img_orig, cv2.COLOR_BGR2RGB)
+img_orig = cv2.imread('resources/center_2022_11_14_10_22_32_396.jpg')
+img_gray = cv2.cvtColor(img_orig, cv2.COLOR_BGR2GRAY)
 
-# set the lower and upper threshold
-med_val = np.median(img_orig)
-lower = int(max(0, .7 * med_val))
-upper = int(min(255, 1.3 * med_val))
+# cv2.imshow('Original image', img_orig)
+# cv2.imshow('Gray image', img_gray)
+#
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
-# blurring with ksize = 3
-img_k3 = cv2.blur(img_orig, ksize=(3, 3))
-# canny detection with different thresholds
-edges_k3 = cv2.Canny(img_k3, threshold1=lower, threshold2=upper)
-edges_k3_2 = cv2.Canny(img_k3, lower, upper + 75)
 
-# blurring with ksize = 5
-img_k5 = cv2.blur(img_orig, ksize=(5, 5))
-# canny detection with different thresholds
-edges_k5 = cv2.Canny(img_k5, lower, upper)
-edges_k5_2 = cv2.Canny(img_k5, lower, upper + 75)
+def edge_detect_imgs(img, kernel_size: tuple, color_space: str):
+    # set the lower and upper threshold
+    med_val = np.median(img)
+    lower = int(max(0, .8 * med_val))
+    upper = int(min(255, 1.3 * med_val))
+    margin = 75
+    upper_higher = upper + margin
 
-# plot the images
-images = [edges_k3, edges_k3_2, edges_k5, edges_k5_2]
-titles = ['blurring, kernel = 3x3, lower threshold',
-          'blurring, kernel = 3x3, higher threshold',
-          'blurring, kernel = 5x5, lower threshold',
-          'blurring, kernel = 5x5, higher threshold']
+    # blurring with ksize = 5
+    img_k3 = cv2.blur(img, ksize=kernel_size)
+    # canny detection with different thresholds
+    edges_k3 = cv2.Canny(img_k3, threshold1=lower, threshold2=upper)
+    edges_k3_2 = cv2.Canny(img_k3, lower, upper_higher)
+
+    # Gaussian blurring with ksize = 5
+    img_k5 = cv2.GaussianBlur(img, ksize=kernel_size, sigmaX=0)
+    # canny detection with different thresholds
+    edges_k5 = cv2.Canny(img_k5, lower, upper)
+    edges_k5_2 = cv2.Canny(img_k5, lower, upper_higher)
+
+    # Return the edge detected images
+    return [
+        [edges_k3, edges_k3_2, edges_k5, edges_k5_2],
+        [f'[{color_space}] blurring, kernel = {kernel_size[0]}x{kernel_size[1]}, threshold = {lower} | {upper}',
+         f'[{color_space}] blurring, kernel = {kernel_size[0]}x{kernel_size[1]}, threshold = {lower} | {upper_higher}',
+         f'[{color_space}] Gaussian blurring, kernel = {kernel_size[0]}x{kernel_size[1]}, threshold = {lower} | {upper}',
+         f'[{color_space}] Gaussian blurring, kernel = {kernel_size[0]}x{kernel_size[1]} threshold = {lower} | {upper_higher}',]
+    ]
+
+
+# plot the images - Colored
+output_color = edge_detect_imgs(img_orig, (5, 5), 'RGB')
+images_color = output_color[0]
+titles = output_color[1]
 plt.figure(figsize=(20, 15))
 for i in range(4):
     plt.subplot(2, 2, i + 1)
-    plt.imshow(images[i])
+    plt.imshow(images_color[i])
+    plt.axis('off')
+    plt.title(titles[i])
+
+plt.show()
+
+
+# plot the images - Gray
+output_gray = edge_detect_imgs(img_gray, (5, 5), 'Gray')
+images_gray = output_gray[0]
+titles = output_gray[1]
+plt.figure(figsize=(20, 15))
+for i in range(4):
+    plt.subplot(2, 2, i + 1)
+    plt.imshow(images_gray[i])
     plt.axis('off')
     plt.title(titles[i])
 
